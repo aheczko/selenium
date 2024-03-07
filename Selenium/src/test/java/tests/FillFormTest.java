aae4186a5345d2f96afa659a01b951c51c94250e
Adrian.Heczko <adrian.heczko@asseco.pl>
2024-03-07 13:03:57 +0100

    Metoda getDriver

--- Selenium/src/test/java/tests/FillFormTest.java
+++ Selenium/src/test/java/tests/FillFormTest.java
@@ -1,33 +1,67 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class FillFormTest {
     private String appurl="http://www.automationpractice.pl/";
-    private String browsername ="Chrome";
+    private String browsername ="chrome";
     //deklaracja sterownika do przeglądarki
     private WebDriver driver;
+    private final boolean headlessBrowser = false; //true - bez widocznej przeglądarki  false - przeglądarka jest widoczna.
+
+
     @Test
     @DisplayName("Contact US form")
     void fillFormForAutomationPractice(){
+        driver = getDriver();
+        driver.get(appurl);
 
+
+        driver.quit();
     }
 
     private WebDriver getDriver()
     {
         switch (this.browsername){
-            case "edge"-> {driver = new EdgeDriver();
+            case "edge"-> {
+                EdgeOptions options = new EdgeOptions();
+                WebDriverManager.edgedriver().setup();
+                options.addArguments("--start-maximized");
+                options.addArguments("--remote-allow-origins=*");
+                if (this.headlessBrowser) {
+                    options.addArguments("--headless");
+                }
+                driver = new EdgeDriver(options);
             }
-            case "chrome" -> {driver = new ChromeDriver();
+            case "chrome" -> {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                WebDriverManager.chromedriver().setup();
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                if (this.headlessBrowser) {
+                    chromeOptions.addArguments("--headless");
+                }
+                driver = new ChromeDriver(chromeOptions);
             }
-            case "firefox" -> {driver = new FirefoxDriver();
+            case "firefox" -> {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                WebDriverManager.firefoxdriver().setup();
+                firefoxOptions.addArguments("start-maximized");
+                if (this.headlessBrowser) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
             }
-            default -> System.out.println("Unsupported driver for web browser");
+            default -> throw new UnsupportedOperationException("Unsupported browser selected.");
         }
         return  driver;
     }

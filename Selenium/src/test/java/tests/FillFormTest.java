e3f8adff105c8aefa32cb1113359a09941246046
Adrian.Heczko <adrian.heczko@asseco.pl>
2024-03-07 17:05:02 +0100

    pierwszy dzień szkolenia

--- Selenium/src/test/java/tests/FillFormTest.java
+++ Selenium/src/test/java/tests/FillFormTest.java
@@ -3,13 +3,22 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FillFormTest {
     private String appurl="http://www.automationpractice.pl/";
@@ -22,12 +31,44 @@ public class FillFormTest {
     @Test
     @DisplayName("Contact US form")
     void fillFormForAutomationPractice(){
+
         driver = getDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get(appurl);
+        //Krok1: Click Contac US
+        WebElement contactLink = driver.findElement(By.cssSelector("#contact-link > a"));
+        contactLink.click();
+        WebElement dropDownList = driver.findElement(By.id("id_contact"));
+        dropDownList.click();
+        Select select = new Select(dropDownList);
+        select.selectByVisibleText("Webmaster");
+        WebElement email = driver.findElement(By.cssSelector("#email"));
+        email.sendKeys("aheczko87@gmail.com");
+        WebElement order = driver.findElement(By.cssSelector("#id_order"));
+        order.sendKeys("Selenium testowanie selenium");
+        WebElement textMsg = driver.findElement(By.cssSelector("#message"));
+        textMsg.sendKeys("testowy tekst wiadomości e mail");
+        File uploadFile = new File("c://opt//info.txt");
+        //WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));
+        WebElement fileInput = driver.findElement(By.cssSelector("#fileUpload"));
+        fileInput.sendKeys(uploadFile.getAbsolutePath());
+        WebElement sendBtn = driver.findElement(By.cssSelector("#submitMessage > span"));
+        sendBtn.click();
+
+
+
+        //Krok 8: assert
+        String expectedMessage ="Your message has been successfully sent to our team.";
+        WebElement succesMsg = driver.findElement(By.className("alert"));
+        String actualMessage = succesMsg.getText();
+        assertThat(actualMessage).contains(expectedMessage);
+
+
 
 
         driver.quit();
     }
+    //"Your message has been successfully sent to our team."
 
     private WebDriver getDriver()
     {
